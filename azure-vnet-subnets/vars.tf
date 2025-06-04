3a049143f9e2f445a52ecc190ec6b06d58e125b7
p.kuligowski@YHXGP2L9HP <p.kuligowski@f5.com>
2025-06-04 08:59:28 +0100

    vars

--- azure-vnet-subnets/vars.tf
+++ azure-vnet-subnets/vars.tf
@@ -1,4 +1,7 @@
 variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
 variable "resource_group_name" {}
 variable "resource_vnet_name" {}
 variable "location" {}
