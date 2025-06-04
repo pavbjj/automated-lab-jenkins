2d5a044dcff868d1993a9f96b620caa54a3fef91
p.kuligowski@YHXGP2L9HP <p.kuligowski@f5.com>
2025-06-04 16:55:10 +0100

    multi-node

---
+++ azure-multi-node-existing-vnet/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "p-kuligowski-rg"
+    storage_account_name = "pkuligowskixc"
+    container_name       = "f5xc"
+    key                  = "jenkins-multi-node-existing-vnet.tfstate"
+  }
+}
