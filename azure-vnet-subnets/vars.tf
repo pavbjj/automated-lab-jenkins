3e8e8b28d6a5125b8b1447c19ee9677b0d703f04
pavbjj <43717447+pavbjj@users.noreply.github.com>
2025-06-04 10:23:50 +0100

    Update vars.tf

--- azure-vnet-subnets/vars.tf
+++ azure-vnet-subnets/vars.tf
@@ -2,6 +2,6 @@ variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
-variable "resource_group_name" {}
-variable "resource_vnet_name" {}
-variable "location" {}
+#variable "resource_group_name" {}
+#variable "resource_vnet_name" {}
+#variable "location" {}
