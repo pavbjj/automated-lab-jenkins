451a9cf8a10a338fbe548aee4a9324c18d39a80a
pavbjj <43717447+pavbjj@users.noreply.github.com>
2025-06-06 08:36:49 +0100

    Update main.tf

--- azure-vnet-subnets/main.tf
+++ azure-vnet-subnets/main.tf
@@ -23,13 +23,6 @@ resource "azurerm_subnet" "outside" {
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.1.0/24"]
-  delegation {
-    name = "natdelegation"
-    service_delegation {
-      name = "Microsoft.Network/natGateways"
-      actions = ["Microsoft.Network/natGateways/*"]
-    }
-  }
 }
 
 resource "azurerm_subnet" "inside" {
@@ -37,28 +30,6 @@ resource "azurerm_subnet" "inside" {
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_public_ip" "nat" {
-  name                = "nat-gateway-public-ip"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_nat_gateway" "nat" {
-  name                = "p-kuligowski-nat-gateway"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  sku_name            = "Standard"
-
-  public_ip_address_id = azurerm_public_ip.nat.id
-}
-
-resource "azurerm_subnet_nat_gateway_association" "outside" {
-  subnet_id      = azurerm_subnet.outside.id
-  nat_gateway_id = azurerm_nat_gateway.nat.id
 }
 
 resource "azurerm_network_security_group" "nsg" {
@@ -80,7 +51,7 @@ resource "azurerm_network_security_group" "nsg" {
 
   security_rule {
     name                       = "Allow-All-Outbound"
-    priority                   = 110
+    priority                   = 100
     direction                  = "Outbound"
     access                     = "Allow"
     protocol                   = "*"
