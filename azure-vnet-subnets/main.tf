33a075bf9eec9ebcb394b6adde5fc79d5d4ef4cc
pavbjj <43717447+pavbjj@users.noreply.github.com>
2025-06-09 08:23:30 +0100

    Update main.tf

--- azure-vnet-subnets/main.tf
+++ azure-vnet-subnets/main.tf
@@ -71,3 +71,32 @@ resource "azurerm_subnet_network_security_group_association" "inside" {
   subnet_id                 = azurerm_subnet.inside.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
+
+# Create Public IP for NAT Gateway
+resource "azurerm_public_ip" "nat" {
+  name                = "p-kuligowski-nat-ip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Create NAT Gateway
+resource "azurerm_nat_gateway" "nat" {
+  name                = "p-kuligowski-nat-gateway"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku_name            = "Standard"
+}
+
+# Associate Public IP to NAT Gateway
+resource "azurerm_nat_gateway_public_ip_association" "nat" {
+  nat_gateway_id = azurerm_nat_gateway.nat.id
+  public_ip_address_id = azurerm_public_ip.nat.id
+}
+
+# Associate NAT Gateway to Inside Subnet (for outbound access)
+resource "azurerm_subnet_nat_gateway_association" "inside" {
+  subnet_id      = azurerm_subnet.outside.id
+  nat_gateway_id = azurerm_nat_gateway.nat.id
+}
